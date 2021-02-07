aa2a26ba78dd6dec331f39769422a4fc216eb02f
Krzysztof Szybowicz <kakaladzekz@gmail.com>
2021-02-07 15:06:05 +0100

    Completed inpost tracking with async functions.

--- server/common/codeVocabs.js
+++ server/common/codeVocabs.js
@@ -1,6 +1,7 @@
 module.exports.InpostPackageTypes = {
     inpost_locker_standard: 'Przesyłka paczkomatowa - standardowa',
     inpost_locker_allegro: 'Przesyłka paczkomatowa - Allegro Paczkomaty InPost.',
+    inpost_locker_allegro_smart: 'Przesyłka paczkomatowa - Allegro Smart Paczkomaty InPost.',
     inpost_locker_pass_thru: 'Przesyłka paczkomatowa - Podaj Dalej',
     inpost_letter_allegro: 'Przesyłka kurierska - Allegro MiniKurier24 InPost',
     inpost_courier_allegro: 'Przesyłka kurierska - Allegro Kurier24 InPost.',
@@ -9,12 +10,9 @@ module.exports.InpostPackageTypes = {
 module.exports.InpostPackageCheck = {
     inpost_locker_standard: 'locker',
     inpost_locker_allegro: 'locker',
+    inpost_locker_allegro_smart: 'locker',
     inpost_locker_pass_thru: 'locker',
     inpost_letter_allegro: 'courier',
     inpost_courier_allegro: 'courier',
     inpost_courier_c2c: 'courier',
-};
-
-module.exports.InpostPackageStatuses = (callback) => {
-    callback('pass');
 };
---
+++ server/common/supportAsyncFunctions.js
@@ -0,0 +1,39 @@
+/* eslint-disable no-plusplus */
+const { InpostPackageTypes, InpostPackageCheck } = require('./codeVocabs.js');
+const InpostPackageStatuses = require('./codeVocabsFunctional.js');
+
+async function parseEventsInpost(events) {
+    const subEventsList = [];
+    const inpostEvents = await InpostPackageStatuses();
+    for (let i = 0; i < events.length; i++) {
+        const tempBody = {
+            status: inpostEvents[events[i].status],
+            date: events[i].datetime,
+        };
+        subEventsList.push(tempBody);
+    }
+    return subEventsList;
+}
+async function parseInpostPackcage(data) {
+    if (InpostPackageCheck[data.service] === 'locker') {
+        const packageData = {
+            packageNumber: data.tracking_number,
+            dateOfPosting: data.created_at,
+            originLockerCode: data.custom_attributes.dropoff_machine_detail.name,
+            destinationLockerCode: data.custom_attributes.target_machine_detail.name,
+            lastUpdate: data.updated_at,
+            serviceName: InpostPackageTypes[data.service],
+            events: await parseEventsInpost(data.tracking_details),
+        };
+        return packageData;
+    }
+    const packageData = {
+        packageNumber: data.tracking_number,
+        dateOfPosting: data.created_at,
+        lastUpdate: data.updated_at,
+        serviceName: InpostPackageTypes[data.service],
+        events: await parseEventsInpost(data.tracking_details),
+    };
+    return packageData;
+}
+module.exports = parseInpostPackcage;
